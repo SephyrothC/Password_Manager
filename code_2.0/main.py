3be0630abea6de94459c07b41155a8e2d702e64c
SephyrothC <as.grifon@gmail.com>
2023-11-02 12:40:07 +0900

    Update main.py

--- code_2.0/main.py
+++ code_2.0/main.py
@@ -1,31 +1,6 @@
 import interface
 
 
-# def main():
-
-#     while True:
-
-#         option = input(
-#             '\nWhat option do you want (type the option number): \n 1 : check my password security \n 2 : create a password\n 3 : transform sentence in password\n(-1 to exit)\n')
-
-#         if option == '-1':  # If the user enters -1
-#             break  # Break out of the loop and end the program
-#         elif option == '1':  # If the user enters 1
-#             # Call the function password_checker with the user's input as a parameter
-#             a = input("Enter your password :\n")
-#             password_checker(a)
-#         elif option == '2':  # If the user enters 2
-#             password_generator()  # Call the function password_generator to generate a new password
-#         elif option == '3':  # If the user enters 3
-#             # Call the function sentence_transformation with the user's input as a parameter
-#             a = input("Enter your sentence :\n")
-#             santence_tranformation(a)
-#         else:
-#             print("Error : input invalid !")
-
-#     exit()
-
-
 if __name__ == '__main__':
     menu = interface.App()
     menu.run()
